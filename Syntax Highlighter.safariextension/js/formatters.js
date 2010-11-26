c50e134a2684ab3c198b3a7d4237c7b2d9e6989f
Rick Fletcher <fletch@pobox.com>
2010-11-26 13:37:04 -0800

    Be explicit about js_beautify formatting options

--- Syntax Highlighter.safariextension/js/formatters.js
+++ Syntax Highlighter.safariextension/js/formatters.js
@@ -1,5 +1,13 @@
 var formatters = {
   javascript: function( src ) {
-    return js_beautify( src );
+    return js_beautify( src, {
+      braces_on_own_line: false,
+      indent_char: " ",
+      indent_level: 0,
+      indent_size: 2,
+      keep_array_indentation: false,
+      preserve_newlines: true,
+      space_after_anon_function: false
+    } );
   }
 };
